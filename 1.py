be9327a12366026bf6a29e6763f2f63d7644abf9
Aurora <109609409+Insearchofimpossible@users.noreply.github.com>
2023-10-08 14:19:39 +0400

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,12 +1,9 @@
 import time
 st = time.time()
-n = 3*10**5
-k = 23
+n,k,temp = 3*10**5,23,0
 a = [x for x in range(1, n + 1)]
-temp = 0
 while len(a) > 1:
     temp = (temp + k - 1) % len(a)
     a.pop(temp)
 print(a)
-end = time.time()
-print(end - st)
+print(time.time() - st)
